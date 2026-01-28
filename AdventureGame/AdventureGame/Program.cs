af8de5d94811ea6262b56d51edac80e132b3ed4e
Andy Wright <agwright05@yahoo.com>
2026-01-28 16:59:18 -0500

    Created commit.

---
+++ AdventureGame/AdventureGame/Program.cs
@@ -0,0 +1,16 @@
+﻿using AdventureGame.Core;
+using System.Security.Claims;
+namespace AdventureGame
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            var myclass = new Class1();
+        }
+
+
+    }
+}
